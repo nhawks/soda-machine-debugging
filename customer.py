731c36f4255b26e60e66af69c8232c870a2d59b6
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-15 16:09:33 -0400

    fixed typo error and improver naming and values

--- customer.py
+++ customer.py
@@ -10,26 +10,26 @@ class Customer:
 
     def gather_coins_from_wallet(self, selected_soda):
         """Method allowing user to choose coins from wallet for payment"""
-        will_proceed = False
+        will_proceed = True
         customer_payment = []
         user_interface.output_text("Continue to add coins until you are ready to insert them into the machine")
         while will_proceed:
             user_interface.display_can_cost(selected_soda)
-            user_interface.display_payment_value(customer_payment)
             coin_name = user_interface.coin_selection()
-            if coin_name == "done":
+            if coin_name == "Done":
                 break
             payment_coin = self.get_wallet_coin(coin_name)
             if payment_coin is not None:
                 customer_payment.append(payment_coin)
             else:
                 user_interface.output_text("You do not have any of those coins, try again")
+            user_interface.display_payment_value(customer_payment)
         return customer_payment
 
     def get_wallet_coin(self, coin_name):
         """Method responsible for retrieving a single coin from wallet's money list"""
         for coin in self.wallet.money:
-            if coin.name == coin.name:
+            if coin.name == coin_name:
                 self.wallet.money.remove(coin)
                 return coin
         return None
