bd13183c5df6b0c2a309395ad2ad34c3ac505f10
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-15 12:23:15 -0400

    add import and fixed indentation

--- wallet.py
+++ wallet.py
@@ -1,4 +1,4 @@
-
+import coins
 
 class Wallet:
     def __init__(self):
@@ -6,13 +6,13 @@ class Wallet:
         self.fill_wallet()
 
 
-def fill_wallet(self):
-    """Method will fill wallet's money list with certain amount of each type of coin when called."""
-    for index in range(8):
-        self.money.append(coins.Quarter())
-    for index in range(10):
-        self.money.append(coins.Dime())
-    for index in range(20):
-        self.money.append(coins.Nickel())
-    for index in range(50):
-        self.money.append(coins.Penny())
+    # """Method will fill wallet's money list with certain amount of each type of coin when called."""
+    def fill_wallet(self):
+        for index in range(8):
+            self.money.append(coins.Quarter())
+        for index in range(10):
+            self.money.append(coins.Dime())
+        for index in range(20):
+            self.money.append(coins.Nickel())
+        for index in range(50):
+            self.money.append(coins.Penny())
